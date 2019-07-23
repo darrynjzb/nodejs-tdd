6c7e8a25385d0b080a302e495a846a5a5758c964
Darryn Josue <darrynjzb@gmail.com>
2019-07-23 12:24:02 -0300

    se instala mongoose

--- server/app.js
+++ server/app.js
@@ -6,6 +6,10 @@ require('dotenv').config();
 const SERVER_HOST = process.env.SERVER_HOST;
 const SERVER_PORT = process.env.SERVER_PORT;
 
+const BD_HOST = process.env.DB_HOST;
+const BD_PORT = process.env.DB_PORT;
+const BD_SCHEMA = process.env.DB_SCHEMA;
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
@@ -18,6 +22,15 @@ app.use(bodyParser.json());
 
 app.get(`${PREFIX}/`, (req, res) => {
     res.send({ data: 'ok' });
+});
+
+const mongoose = require('mongoose');
+mongoose.connect(`mongodb://${BD_HOST}:${BD_PORT}/${BD_SCHEMA}`, {useNewUrlParser: true}, (err, res) => {
+    if (err) {
+        throw new Error('No se pudo conectar a la base de datos');
+    }
+
+    console.log('conectado a la base de datos');
 });
 
 app.listen(SERVER_PORT, SERVER_HOST, () => {
